c28a33d45aa60a3e66cf5117b9f84394113a9bc9
Manuton04 <manutorres004@gmail.com>
2024-01-22 13:34:05 -0300

    Fixed bug when using plugin without PlaceholderAPI

--- src/main/java/fp/manuton/utils/MessageUtils.java
+++ src/main/java/fp/manuton/utils/MessageUtils.java
@@ -2,9 +2,12 @@ package fp.manuton.utils;
 
 import fp.manuton.FarmingPlus;
 import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+
+import java.util.UUID;
 
 public class MessageUtils {
 
@@ -22,10 +25,38 @@ public class MessageUtils {
 
         if (p != null)
             message = message.replace("%player%", p.getName());
-        message = PlaceholderAPI.setPlaceholders(p, message);
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            message = PlaceholderAPI.setPlaceholders(p, message);
+        }else{
+            if (message.contains("%farmingplus_prefix%"))
+                message = message.replace("%farmingplus_prefix%", FarmingPlus.prefix);
+            if (message.contains("%farmingplus_total_rewards%"))
+                message = message.replace("%farmingplus_total_rewards%", String.valueOf(FarmingPlus.getPlugin().getMainConfigManager().getRewardsCounter(p.getUniqueId()).getTotal()));
+            if (message.contains("%farmingplus_top_position%"))
+                message = message.replace("%farmingplus_top_position%", String.valueOf(FarmingPlus.getPlugin().getMainConfigManager().getTopPlayer(p)));
+            if (message.contains("%farmingplus_total_rewards_top_")){ // Usage: %farmingplus_total_rewards_top_number%
+                String[] parts = message.split("_");
+                String positionString = parts[parts.length-1].replace("%", "");
+                int position = Integer.parseInt(positionString);
+                boolean hasPlayer = false;
+                if (p != null) {
+                    for (UUID u : FarmingPlus.getPlugin().getMainConfigManager().getAllRewardsCounterUuids()) {
+                        if (p.getUniqueId().equals(u)) {
+                            hasPlayer = true;
+                            break;
+                        }
+                    }
+                }
+                if (!hasPlayer)
+                    message = FarmingPlus.getPlugin().getMainConfigManager().getNoTop().replace("%farmingplus_top_number%", String.valueOf(position));
+                if (FarmingPlus.getPlugin().getMainConfigManager().getTopPlayer(position) == null)
+                    message = FarmingPlus.getPlugin().getMainConfigManager().getNoTop().replace("%farmingplus_top_number%", String.valueOf(position));
+                else
+                    message =FarmingPlus.getPlugin().getMainConfigManager().getTopPlayer(position);
+            }
+        }
         message = getColoredMessage(message);
         return message;
-
     }
 
     public static boolean isStringEmpty(String string){
